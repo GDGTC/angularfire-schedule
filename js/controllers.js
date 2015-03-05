5b4550964cc1e0161a70f7ff6f54f1acfceaf287
Stephen Fluin <stephen.fluin@gmail.com>
2015-03-05 16:30:35 -0600

    Updated version of angular and firebase. Added schedule file for testing. Integrated firebase data.

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
 var app = angular.module('ScheduleControllers', []);
 
-app.controller('HomeCtrl',function($scope, Navigate) {
+app.controller('HomeCtrl', function($scope, Navigate, $firebase, $firebaseArray) {
 	$scope.goto = Navigate.goto;
 
 	$scope.tracks = 
@@ -13,66 +13,20 @@ app.controller('HomeCtrl',function($scope, Navigate) {
 		 "design":"design & frontend",
 		 "starting":"getting started"};
 
-    sessions =
-    [
-    	{
-			speakers: [{name:'Speaker Name', bio:'Bio',twitter:'@handle'}],
-			title: 'Check-in / Registration',
-			description: 'coffee & donuts',
-			room: 'Atrium',
-			track: 'general',
-			time: '8:00AM'
-		},
-		{
-			speakers: [{name:'Aaron Goldberg', bio:'Bio',twitter:'@handle'} ],
-			title: 'RecyclerView',
-			description: 'Come and learn everything!',
-			room: 'Room 3',
-			track: 'android',
-			time: '8:45AM'
-		},
-		{
-			speakers: [{name:'Brian Yencho', bio:'Bio',twitter:'@handle'} ],
-			title: 'Apps for Android Wear',
-			description: 'Come and learn everything!',
-			room: 'Room 3',
-			track: 'wearables',
-			time: '8:45AM'
-		},
-		{
-			speakers: [{name:'Ilya Verbitskiy', bio:'Bio',twitter:'@handle'} ],
-			title: 'WebRTC Workshop',
-			description: 'Come and learn everything!',
-			room: 'Room 3',
-			track: 'chrome',
-			time: '8:45AM'
-		},
-		{
-			speakers: [{name:'Noah Blon', bio:'Bio',twitter:'@handle'} ],
-			title: 'Material Design / Front End',
-			description: 'Come and learn everything!',
-			room: 'Room 3',
-			track: 'design',
-			time: '8:45AM'
-		},
-		{
-			speakers: [{name:'Stephen Fluin', bio:'Bio',twitter:'@handle'} ],
-			title: 'AngularJS Workshop',
-			description: 'Come and learn everything!',
-			room: 'Room 3',
-			track: 'starting',
-			time: '8:45AM'
-		}
+    var ref = new Firebase("https://devfestmn.firebaseio.com/sessions");
+    $scope.sessions = $firebaseArray(ref);
 
-    ];
+    $scope.sessions.$watch(function(event) {
+    	console.log("Sessoins loaded, reworking data.");
+    	$scope.sessionTimes = {};
+	    // Organize the sessions into timeboxes
+	    for (var i = $scope.sessions.length - 1; i >= 0; i--) {
+	    	if(!$scope.sessionTimes[$scope.sessions[i].time] ) {
+	    		$scope.sessionTimes[$scope.sessions[i].time] = {"sessions":[]};
+	    	}
+	    	$scope.sessionTimes[$scope.sessions[i].time].sessions.push($scope.sessions[i]);
+	    };
+    });
 
-    $scope.sessionTimes = {};
-    // Organize the sessions into timeboxes
-    for (var i = sessions.length - 1; i >= 0; i--) {
-    	if(!$scope.sessionTimes[sessions[i].time] ) {
-    		$scope.sessionTimes[sessions[i].time] = {"sessions":[]};
-    	}
-    	$scope.sessionTimes[sessions[i].time].sessions.push(sessions[i]);
-    };
-
+    
 });

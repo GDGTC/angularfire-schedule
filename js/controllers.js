3cbb08e2db52fd44d6509aee7b27e71f0d98f695
Stephen Fluin <stephen.fluin@gmail.com>
2015-03-05 15:39:49 -0600

    playing with formatting and looping

--- js/controllers.js
+++ js/controllers.js
@@ -11,5 +11,37 @@ app.controller('HomeCtrl',function($scope, Navigate) {
 		 "wearables":"wearables",
 		 "chrome":"chrome & cloud",
 		 "design":"design & frontend",
-		 "starting":"getting started"}
+		 "starting":"getting started"};
+
+    sessions =
+    [
+    	{
+			id: 3,
+			speakers: [{name:'Speaker Name', bio:'Bio',twitter:'@handle'}],
+			title: 'Check-in / Registration',
+			description: 'coffee & donuts',
+			room: 'Atrium',
+			track: 'general',
+			time: '8AM'
+		},
+		{
+			id: 3,
+			speakers: [{name:'Speaker Name', bio:'Bio',twitter:'@handle'} ],
+			title: 'Android Fun',
+			description: 'Come and learn everything!',
+			room: 'Room 3',
+			track: 'android',
+			time: '8:45AM'
+		}
+
+    ];
+
+    $scope.sessionTimes = {};
+    // Organize the sessions into timeboxes
+    for (var i = sessions.length - 1; i >= 0; i--) {
+    	if(!$scope.sessionTimes[sessions[i].time] ) {
+    		$scope.sessionTimes[sessions[i].time] = {"sessions":[]};
+    	}
+    	$scope.sessionTimes[sessions[i].time].sessions.push(sessions[i]);
+    };
 });
